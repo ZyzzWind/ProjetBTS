ccdcc41ac7ea8ca9263a6959dcbf5fcebf59762f
TchouTchouuu <60777889+TchouTchouuu@users.noreply.github.com>
2020-02-09 12:19:46 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="bouttons.css">
+</head>
+<body>
+
+<a href="index.php" class="button_home"></a>
+<a href="RoseDesVents.xhtml" class="button_speed_direction"></a>
+<a href="EolienneSimulation.php" class="button_simulation"></a>
+<a href="EolienneProgress.php" class="button_progress"></a>
+<a href="ComparaisonEoliennes.php" class="button_compare_windmill"></a>
+<a href="ComparaisonLieuxEoliennes.php" class="button_compare_places"></a>
+</body>
+</html>
