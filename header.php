5995e1f39b7083e105d21cbf4462d324ceea9252
TchouTchouuu <60777889+TchouTchouuu@users.noreply.github.com>
2020-02-18 15:20:12 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -12,12 +12,7 @@
         echo "<p>Connecté en tant que ".$_SESSION['user']."</p>";
         echo "</div>";
     }
-    else
-    {
-        echo "<div class='etat_connexion'>";
-        echo "<p>Non connecté</p>";
-        echo "</div>";
-    }
+
 ?>
 <a href="index.php" class="button_home"></a>
 <a href="RoseDesVents.php" class="button_speed_direction"></a>
