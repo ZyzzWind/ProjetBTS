7d12a98bffa0ef8d9a279a077782ac1bffb75791
ZyzzWind <60777837+ZyzzWind@users.noreply.github.com>
2020-03-13 13:32:55 +0100

    Add files via upload

--- windmeter.sql
+++ windmeter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 21 fév. 2020 à 12:43
+-- Généré le :  ven. 13 mars 2020 à 12:26
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -30,11 +30,48 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `donnees_vent`;
 CREATE TABLE IF NOT EXISTS `donnees_vent` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `iddonnees_vent` int(11) NOT NULL AUTO_INCREMENT,
   `time` datetime NOT NULL,
   `wind_speed_avg` float NOT NULL,
   `wind_heading` float NOT NULL,
-  PRIMARY KEY (`id`)
+  `lieux_idlieux` int(11) NOT NULL,
+  PRIMARY KEY (`iddonnees_vent`),
+  KEY `fk_donnees_vent_lieux_idx` (`lieux_idlieux`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `eoliennes`
+--
+
+DROP TABLE IF EXISTS `eoliennes`;
+CREATE TABLE IF NOT EXISTS `eoliennes` (
+  `ideoliennes` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `rayon` float NOT NULL,
+  `vitesse_max` float NOT NULL,
+  `vitesse_min` float NOT NULL,
+  `prix` float NOT NULL,
+  `puissance_nominale` float NOT NULL,
+  `temps_de_reponse` float NOT NULL,
+  PRIMARY KEY (`ideoliennes`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lieux`
+--
+
+DROP TABLE IF EXISTS `lieux`;
+CREATE TABLE IF NOT EXISTS `lieux` (
+  `idlieux` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pioupiou` int(11) NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  PRIMARY KEY (`idlieux`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,19 +82,12 @@ CREATE TABLE IF NOT EXISTS `donnees_vent` (
 
 DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE IF NOT EXISTS `utilisateurs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(24) NOT NULL,
+  `idutilisateurs` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
   `mail` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `utilisateurs`
---
-
-INSERT INTO `utilisateurs` (`id`, `user`, `mail`, `password`) VALUES
-(1, 'Hugo', 'pv.hugom@gmail.com', 'mdp');
+  PRIMARY KEY (`idutilisateurs`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
